404a1e161fcb164fd1cd095b894763f07cd086b7
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-19 21:26:25 -0700

    added more urls an views to make full crud for homes

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('about/', views.About.as_view(), name="about"), # <- new route
     path('homes/', views.HomeList.as_view(), name="homelist"),
     path('homes/create', views.Home_Create.as_view(), name="home_create"),
-    # path('homes/<int:pk>/', views.Home_Detail.as_view(), name="home_detail"),
-    # path('homes/<int:pk>/update', views.Home_Update.as_view(), name="home_update"),
+    path('homes/<int:pk>/', views.Home_Detail.as_view(), name="home_detail"),
+    path('homes/<int:pk>/update', views.Home_Update.as_view(), name="home_update"),
     # path('homes/<int:pk>/delete', views.Home_Delete.as_view(), name="home_delete"),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
+from audioop import reverse
 from re import template
 from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
 from .models import Home
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DetailView
+from django.urls import reverse
 
 # Create your views here.
 
@@ -33,4 +36,18 @@ class Home_Create(CreateView):
     model = Home
     fields = ['location', 'home_type', 'amenities', 'image', 'contact_name', 'contact_email', 'bedrooms', 'available']
     template_name = "home_create.html"
-    success_url = "/homes/"
+    def get_success_url(self):
+        return reverse('home_detail', kwargs={'pk': self.object.pk})
+
+
+class Home_Detail(DetailView):
+    model = Home
+    template_name = "home_detail.html"
+
+
+class Home_Update(UpdateView):
+    model = Home
+    fields = ['location', 'home_type', 'amenities', 'image', 'contact_name', 'contact_email', 'bedrooms', 'available']
+    template_name = "home_update.html"
+    def get_success_url(self):
+        return reverse('home_detail', kwargs={'pk': self.object.pk})
