6a3bbd4111654242dcbcd128e421bf5a29f9abbd
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-20 20:02:24 -0700

    added reviews

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Home, Car # import the Home model from models.py
+from .models import Home, Car, Review # import the Home model from models.py
 # Register your models here.
 
 admin.site.register(Home)
 admin.site.register(Car)
+admin.site.register(Review)
