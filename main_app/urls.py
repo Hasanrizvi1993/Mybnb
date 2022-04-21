d7cdd7de194aaa121ba6b1f179f242faea4f229c
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-20 22:25:03 -0700

    added urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
+#searchbar
+    path('search_homes/', views.search_homes, name='search-homes '),
 ]
 
 
