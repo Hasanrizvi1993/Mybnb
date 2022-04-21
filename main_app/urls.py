b498d4f5fae9dcfdcf1b80362c333e5aa4dc9a85
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-20 18:49:02 -0700

    added login,signup,logout urls to the utls.py page

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,6 +17,10 @@ urlpatterns = [
     path('cars/create/', views.CarCreate.as_view(), name='car_create'),
     path('cars/<int:pk>/update/', views.CarUpdate.as_view(), name='car_update'),
     path('cars/<int:pk>/delete/', views.CarDelete.as_view(), name='car_delete'),
+#auth
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
 
 
